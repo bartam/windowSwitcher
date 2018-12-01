216c538f592d8c337b8d188e72e8592e28a19be9
Mark Barta <barta.mark@gmail.com>
2018-12-01 09:06:43 -0500

    init

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+
 
 namespace UI
 {
@@ -23,6 +28,219 @@ namespace UI
         public MainWindow()
         {
             InitializeComponent();
+            UpdateProcessList();
+
+            //this.ProcessListView.Items.Add(new Proc { Command = "", Name = "David" });
+        }
+
+        private void UpdateProcessList(int index = 0)
+        {
+            this.ProcessListView.SelectedItem = null;
+            this.ProcessListView.Items.Clear();
+
+            var procList = Properties.Settings.Default.Processes.Cast<string>().ToArray();
+            var nameList = Properties.Settings.Default.Names.Cast<string>().ToArray();
+
+            var col = new List<Proc>();
+
+            procList.ToList().ForEach(x => {
+                var ix = procList.ToList().IndexOf(x);
+
+                var proc = x;
+                var name = nameList.ToList().ElementAt(ix);
+
+                
+                col.Add(new Proc { Command = proc, Name = name });
+
+                //this.ProcessListView.Items.Add(new Proc { Command = proc, Name = name });
+            });
+
+            List<Proc> SortedList = col.OrderBy(o => o.Name).ToList();
+
+            SortedList.ForEach(x => {
+                this.ProcessListView.Items.Add(new Proc { Command = x.Command, Name = x.Name });
+            });
+
+
+            this.ProcessListView.SelectedIndex = index;
+        }
+
+
+        private void SetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var command = this.ProcessCommandTextBox.Text;
+            var name = this.NicknameTextBox.Text;
+
+            Properties.Settings.Default.Processes.Add(command);
+            Properties.Settings.Default.Names.Add(name);
+
+            this.ProcessListView.Items.Add(new Proc { Command = command, Name = name });
+            Properties.Settings.Default.Save();
+            Console.WriteLine("Added");
+        }
+
+
+        private void ProcessListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(this.ProcessListView.SelectedItem != null) {
+                Console.WriteLine("Selection Changed");
+                var selection = (Proc)this.ProcessListView.SelectedItem;
+                this.SelectedItemNickNameTextBox.Text = selection.Name.ToString();
+                this.SelectedItemCommandTextBox.Text = selection.Command.ToString();
+
+                //To minimize the period of time where there's no window active
+                //Lets Maximize the selected window first, then minimize the rest
+
+                ShowWindow.restore(selection.WindowHandle);
+
+                var list = this.ProcessListView.Items;
+                foreach (Proc item in list) {
+                    if (item.WindowHandle != System.IntPtr.Zero) {
+                        if(selection.WindowHandle == item.WindowHandle) {
+                            //Console.WriteLine("Found Handle");
+                            //ShowWindow.maximize(item.WindowHandle);
+                        } else {
+                            ShowWindow.minimize(item.WindowHandle);
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+
+        private void StartProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selection = (Proc)this.ProcessListView.SelectedItem;
+            if( selection.WindowHandle != System.IntPtr.Zero ) {
+                var split = selection.Command.Split();
+
+                var parsePath = ParseFilePath(split[0]);
+                var arguments = selection.Command.Remove(0, selection.Command.IndexOf(' ') + 1);
+
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = parsePath[1];
+                startInfo.Arguments = arguments;
+                startInfo.CreateNoWindow = false;
+                startInfo.UseShellExecute = true;
+                startInfo.WorkingDirectory = parsePath[0];
+
+                var process = Process.Start(startInfo);
+                process.WaitForInputIdle();
+                var handle = process.MainWindowHandle;
+
+                selection.WindowHandle = handle;
+            }
+        }
+
+        private string[] ParseFilePath(string command)
+        {
+            string s = command;
+            //int idx = s.LastIndexOf('.');
+            int idx = s.LastIndexOfAny(new char[] { '\\', '/' });
+
+            //if (idx != -1) {
+            //    Console.WriteLine(s.Substring(0, idx)); // "My. name. is Bond"
+            //    Console.WriteLine(s.Substring(idx + 1)); // "_James Bond!"
+            //}
+
+            var arr = new string[2];
+            arr[0] = command.Substring(0, idx);
+            arr[1] = command.Substring(idx + 1);
+
+            return arr;
+        }
+
+        private void SelectedItemSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selection = (Proc)this.ProcessListView.SelectedItem;
+
+            var oldCommand = selection.Command.ToString();
+            var oldName = selection.Name.ToString();
+
+            var command = this.SelectedItemCommandTextBox.Text;
+            var name = this.SelectedItemNickNameTextBox.Text;
+
+            
+
+            //Edit Command
+            var indexOfCommand = Properties.Settings.Default.Processes.IndexOf(oldCommand);
+            if(indexOfCommand > -1) {
+                Properties.Settings.Default.Processes[indexOfCommand] = command;
+            }
+            //Edit Name
+            var indexOfName = Properties.Settings.Default.Names.IndexOf(oldName);
+            if(indexOfName > -1) {
+                Properties.Settings.Default.Names[indexOfName] = name;
+            }
+
+
+            Properties.Settings.Default.Save();
+
+            UpdateProcessList(this.ProcessListView.SelectedIndex);
+
+        }
+
+        private void SelectedItemDeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selection = (Proc)this.ProcessListView.SelectedItem;
+
+            var oldCommand = selection.Command.ToString();
+            var oldName = selection.Name.ToString();
+
+            //Remove Command
+            var indexOfCommand = Properties.Settings.Default.Processes.IndexOf(oldCommand);
+            if (indexOfCommand > -1) {
+                Properties.Settings.Default.Processes.RemoveAt(indexOfCommand);
+            }
+            //Remove Name
+            var indexOfName = Properties.Settings.Default.Names.IndexOf(oldName);
+            if (indexOfName > -1) {
+                Properties.Settings.Default.Names.RemoveAt(indexOfName);
+            }
+
+            Properties.Settings.Default.Save();
+            UpdateProcessList();
+
+        }
+
+        private void StartAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            var list = this.ProcessListView.Items;
+            Console.WriteLine("click");
+            foreach( Proc item in list) {
+                if(item.WindowHandle == System.IntPtr.Zero) {
+                    var split = item.Command.Split();
+
+                    var parsePath = ParseFilePath(split[0]);
+                    var arguments = item.Command.Remove(0, item.Command.IndexOf(' ') + 1);
+
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = parsePath[1];
+                    startInfo.Arguments = arguments;
+                    startInfo.CreateNoWindow = false;
+                    startInfo.UseShellExecute = true;
+                    startInfo.WorkingDirectory = parsePath[0];
+
+                    var process = Process.Start(startInfo);
+                    process.WaitForInputIdle();
+                    var handle = process.MainWindowHandle;
+
+                    item.WindowHandle = handle;
+
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
         }
     }
+}
+
+public class Proc
+{
+    public string Name { get; set; }
+
+    public string Command { get; set; }
+
+    public System.IntPtr WindowHandle { get; set; }
 }
---
+++ UI/Util/ShowWindow.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace UI
+{
+    class ShowWindow
+    {
+        // Define API functions.
+        //[DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        //static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetWindowPlacement(IntPtr hWnd, out WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPlacement(IntPtr hWnd,
+           [In] ref WINDOWPLACEMENT lpwndpl);
+
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WINDOWPLACEMENT
+        {
+            public int Length;
+            public int Flags;
+            public ShowWindowCommands ShowCmd;
+            public POINT MinPosition;
+            public POINT MaxPosition;
+            public RECT NormalPosition;
+            public static WINDOWPLACEMENT Default
+            {
+                get {
+                    WINDOWPLACEMENT result = new WINDOWPLACEMENT();
+                    result.Length = Marshal.SizeOf(result);
+                    return result;
+                }
+            }
+        }
+
+        internal enum ShowWindowCommands : int
+        {
+            Hide = 0,
+            Normal = 1,
+            ShowMinimized = 2,
+            Maximize = 3, // is this the right value?
+            ShowMaximized = 3,
+            ShowNoActivate = 4,
+            Show = 5,
+            Minimize = 6,
+            ShowMinNoActive = 7,
+            ShowNA = 8,
+            Restore = 9,
+            ShowDefault = 10,
+            ForceMinimize = 11
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            private int _Left;
+            private int _Top;
+            private int _Right;
+            private int _Bottom;
+        }
+
+        // Set the target's placement.
+        static public void setWindow(IntPtr handler, int status)
+        {
+            // Get the target window's handle.
+            //IntPtr target_hwnd = FindWindowByCaption(IntPtr.Zero, txtAppTitle.Text);
+            //if (target_hwnd == IntPtr.Zero) {
+            //    MessageBox.Show("Could not find a window with the title \"" +
+            //        txtAppTitle.Text + "\"");
+            //    return;
+            //}
+
+            // Prepare the WINDOWPLACEMENT structure.
+            WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+            placement.Length = Marshal.SizeOf(placement);
+
+            // Get the window's current placement.
+            GetWindowPlacement(handler, out placement);
+
+            // Set the placement's action.
+            //if (radMaximized.Checked)
+            //    placement.ShowCmd = ShowWindowCommands.ShowMaximized;
+            //else if (radMinimized.Checked)
+            //    placement.ShowCmd = ShowWindowCommands.ShowMinimized;
+            //else
+            //    placement.ShowCmd = ShowWindowCommands.Normal;
+
+            placement.ShowCmd = (ShowWindowCommands)status;
+
+            // Perform the action.
+            SetWindowPlacement(handler, ref placement);
+        }
+
+        static public void minimize(IntPtr handle)
+        {
+            setWindow(handle, 6);
+        }
+
+        static public void maximize(IntPtr handle)
+        {
+            setWindow(handle, 3);
+        }
+
+        static public void restore (IntPtr handle)
+        {
+            setWindow(handle, 9);
+        }
+    }
+}
